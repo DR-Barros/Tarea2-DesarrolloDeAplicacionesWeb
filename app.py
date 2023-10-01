9eab9e0487a28e2ff60a2f78852b299af286f9da
Daniel <danielradriganb@gmail.com>
2023-10-01 11:54:37 -0300

    REFACTOR(TEMPLATE): se cambain las templates para reutilizar menu y footer usando base.html

--- app.py
+++ app.py
@@ -6,5 +6,30 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/agregar-hincha')
+def agregarHincha():
+    return render_template('agregar-hincha.html')
+
+@app.route('/agregar-artesano')
+def agregarArtesano():
+    return render_template('agregar-artesano.html')
+
+@app.route('/informacion-hincha')
+def informacionHincha():
+    return render_template('informacion-hincha.html')
+
+@app.route('/informacion-artesano')
+def informacionArtesano():
+    return render_template('informacion-artesano.html')
+
+@app.route('/ver-hinchas')
+def verHinchas():
+    return render_template('ver-hinchas.html')
+
+@app.route('/ver-artesanos')
+def verArtesanos():
+    return render_template('ver-artesanos.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
